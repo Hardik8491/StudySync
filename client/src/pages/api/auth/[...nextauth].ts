40ecf68efd24563f95ab814a6f9fb4a4b58c167b
Hardik Bhammar <hardikbhammar808@gmail.com>
2024-10-27 12:22:29 +0530

    update

--- client/src/pages/api/auth/[...nextauth].ts
+++ client/src/pages/api/auth/[...nextauth].ts
@@ -15,7 +15,7 @@ export const authOptions:NextAuthOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     }),
   ],
-  NEXTAUTH_SECRET: process.env.SECRET as string,
+  secret: process.env.SECRET as string,
 };
 
 export default NextAuth(authOptions);
